2d767ee765f7d31093f451182012e10a3dd69ca6
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2016-06-11 18:19:05 +0900

    Add Jump Scene

---
+++ Assets/Scripts/JumpScene/Jump.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+	private bool flag = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (!flag && Input.GetKey (KeyCode.Return)) {
+			transform.position += new Vector3 (0.0f, 1.0f, -2.0f);
+			flag = true;
+		}
+		if (flag && transform.eulerAngles.x < 90.0f) {
+			transform.Rotate (new Vector3 (1f, 0f, 0f));
+		}
+	}
+}
--- Assets/Scripts/StartScene/StartScene.cs
+++ Assets/Scripts/StartScene/StartScene.cs
@@ -5,7 +5,10 @@ public class StartScene : MonoBehaviour {
 
 	// Use this for initialization
 	void Update() {
-		if (Input.GetKey(KeyCode.Return))
+		if (Input.GetKey (KeyCode.Alpha1)) {
 			Application.LoadLevel ("FallScene");
+		} else if (Input.GetKey (KeyCode.Alpha2)) {
+			Application.LoadLevel ("JumpScene");
+		}
 	}
 }
