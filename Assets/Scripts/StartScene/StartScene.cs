dd3a52b70ea68202eea279581dbbb0faddc78488
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2016-06-11 17:20:34 +0900

    タイトルシーンを追加

---
+++ Assets/Scripts/StartScene/StartScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Update() {
+		if (Input.GetKey(KeyCode.Return))
+			Application.LoadLevel ("FallScene");
+	}
+}
