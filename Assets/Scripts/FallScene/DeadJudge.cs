60cb5421c15502dc883e3bc44a3f73334e39538a
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2016-06-12 12:10:06 +0900

    FallSceneに落下音を追加

---
+++ Assets/Scripts/FallScene/DeadJudge.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadJudge : MonoBehaviour {
+	public GameObject sound;
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.CompareTag("floor")) {
+			sound.SendMessage("Play");
+			Application.LoadLevel ("DeadScene");
+		}
+	}
+}
---
+++ Assets/Scripts/FallScene/FallSound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallSound : MonoBehaviour {
+	private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this);
+		audio = GetComponent<AudioSource> ();
+	}
+
+	public void Play () {
+		audio.PlayOneShot (audio.clip);
+	}
+}
